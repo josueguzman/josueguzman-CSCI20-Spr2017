adafaa66f6ddff28b4b3a82d762a9cf08ba40858
Josue Guzman <jguzman019@student.butte.edu>
2017-01-31 22:45:06 +0000

    lab.12 commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+
+cout <<"MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"<<endl;
+cout <<"MMMMMMMMMMMMMMMMMMMMMN0occd0NMMMMMMMMMMMMMMMMMMMMM"<<endl;
+cout <<"MMMMMMMMMMMMMMMWNKOdc,.    .,cdkKXNWMMMMMMMMMWMMMM"<<endl;
+cout <<"MMWOllloodollc:;'.            ...';:::clooc::cOWMM"<<endl;
+cout <<"MMWl   .lxo,  cx: .l:.;dddl..dk:.:kkl..okc.   oWMM"<<endl;
+cout <<"MMWl   .kXKl :0X0;.xd.l00K0;,OXl.lKKx..xXd.   lWMM"<<endl;
+cout <<"MMWo    ',,. .,',. '. .;,,.  ',. .''.  ',.    oWMM"<<endl;
+cout <<"MMWd.     .,.      .';,.  .;;'.      ,:.      dWMM"<<endl;
+cout <<"MMMk.    .cxx;    ,k0Kk'  :0K0x'   .okl,.    .kMMM"<<endl;
+cout <<"MMM0'   .c:.cko;.'xKKKx.  ,OKKKo,':xd'.l;    '0MMM"<<endl;
+cout <<"MMMNc    'c:;cdxcl0KKKx.  'kKKKOllOd:;c;.    :XMMM"<<endl;
+cout <<"MMMWx.     .. .;x0K000x;..;x00K0xl:. .      .dWMMM"<<endl;
+cout <<"MMMMX;         .x0kxxkkkxxodkOOOd,          ,KMMMM"<<endl;
+cout <<"MMMMWk.        .dOl'...l0XKkx00xo;.        .dWMMMM"<<endl;
+cout <<"MMMMMNl         ;xdolllxKNNNNW0do;         :XMMMMM"<<endl;
+cout <<"MMMMMMK:        .clkWNK0KNNWMWOdc.        ,0MMMMMM"<<endl;
+cout <<"MMMMMMMK;        .:l0NKXXXNWNOoc.        ,OMMMMMMM"<<endl;
+cout <<"MMMMMMMMK:     .'cl:l0NNNNWXkolddc'     ,0MMMMMMMM"<<endl;
+cout <<"MMMMMMMMMXl. .:oxxc'..;;;:cccol:cddc. .cKMMMMMMMMM"<<endl;
+cout <<"MMMMMMMMMMWO;...    .'..;ccll;.      ,xNMMMMMMMMMM"<<endl;
+cout <<"MMMMMMMMMMMMNx,        .''..       'dXMMMMMMMMMMMM"<<endl;
+cout <<"MMMMMMMMMMMMMMNkc.              .:xXMMMMMMMMMMMMMM"<<endl;
+cout <<"MMMMMMMMMMMMMMMMWXkl,.      .'cxKWMMMMMMMMMMMMMMMM"<<endl;
+cout <<"MMMMMMMMMMMMMMMMMMMMWKOxoodkKNMMMMMMMMMMMMMMMMMMMM"<<endl;
+cout <<"MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM"<<endl;
+
+return 0;
+
+}
