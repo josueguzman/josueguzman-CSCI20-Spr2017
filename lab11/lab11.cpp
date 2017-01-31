ab348c2e0e368acb6cc1aabfcf0a7cb2c8268b0b
Josue Guzman <jguzman019@student.butte.edu>
2017-01-31 21:21:38 +0000

    lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,24 @@
+This is the Simple Guessing Game
+
+game instructions:
+
+Click the button to create randomly selected number
+
+select a number between a and b
+
+guess the number
+enter the number and push enter
+
+if the you guess correctly, you win!
+if you don't guess correctly, the program will tell you higher or lower
+Then you'll guess again until you get the right number
+
+Scoring
+player will lose 1 point from 10 for each incorrect guess
+
+compuer instructions:
+create a button that generates a number between number a and b
+create a box for the user to enter the number
+verify if the number is correct
+if the number is correct the first time offer 10 points
+if the number is incorrect, deduct 1 point per guess until correct
