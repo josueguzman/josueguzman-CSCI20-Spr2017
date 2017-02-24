56c257a5dbbc9ffed93cf5b129fadca665c9d913
Josue Guzman <jguzman019@student.butte.edu>
2017-02-24 07:54:36 +0000

    lab22 submission

---
+++ lab22/lab22.cpp
@@ -0,0 +1,18 @@
+/* this program generates numbers from 0 to 100.
+ * It uses rand() % 100 to generate the numbers.
+ * Lab 2.2 | Created By: Josue Guzman | Date: 2/23/17
+ */
+
+#include <iostream>
+#include <cstdlib>     // allows the use of the rand() function
+#include <ctime>       // allows the use of the time() function
+using namespace std;
+
+int main() {
+   srand(time(0));     
+
+   cout << "This program generates random numbers." << '\n';
+   cout << "Hit the RUN button continuosly to get new numbers:" << '\n';
+   cout << ((rand() % 100) + 1) << '\n';
+
+}
