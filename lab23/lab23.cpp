6c61637be37d22a05ba8d65fad6c97a45faf21ea
Josue Guzman <jguzman019@student.butte.edu>
2017-03-01 07:58:30 +0000

    lab23 submission

---
+++ lab23/lab23.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>     
+#include <ctime>       
+#include <string>
+using namespace std;
+
+
+int main() {
+   
+void swap (int num1, int num2, int num3) {
+  int x;
+  int y;
+  
+  x = num1;
+  y = num2;
+  z = num3;
+  num1 = y;
+  num2 = x;
+  num3 = z;
+  
+}
+
+void randnum (int num1, int num2, int num3) {
+srand(time(0));     // random number generator
+    
+}
+
+   return 0;
+}
