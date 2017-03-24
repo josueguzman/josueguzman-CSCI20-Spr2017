a8ccc36f14d610ba82e116c3dd5a8a4464f72b0f
Josue Guzman <jguzman019@student.butte.edu>
2017-03-24 05:44:29 +0000

    lab31 submission

---
+++ lab31/lab31.cpp
@@ -0,0 +1,10 @@
+/* Lab 31 - Conditions
+ * Created By: Josue Guzman
+ * Created On: March 23, 2017
+ * This program creates a new cell phone company that uses complicated formulas
+   to determine the charges for their service.  
+ */
+ 
+#include<iostream>
+using namespace std;
+
